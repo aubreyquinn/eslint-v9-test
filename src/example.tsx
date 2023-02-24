b7966c3657355925c9cb858fb94cf6b7f240592b
Aubrey Quinn <80953505+aubreyquinn@users.noreply.github.com>
2023-02-24 15:13:49 +0000

    Initial commit

---
+++ src/example.tsx
@@ -0,0 +1,6 @@
+import { Button, ButtonProps } from "@fluentui/react-components";
+import * as React from "react";
+
+export const Default = (props: ButtonProps) => (
+  <Button {...props}>Example</Button>
+);
